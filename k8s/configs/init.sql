9d7bdec2eb4a09c31f07e6e1250a1cf52160c7bf
nsce9806q <nsce9806q@naver.com>
2025-02-20 22:24:49 +0900

    changed db configs

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -1,7 +1,7 @@
-CREATE SCHEMA IF NOT EXISTS `mysql` DEFAULT CHARACTER SET utf8mb4;
+CREATE SCHEMA IF NOT EXISTS `db` DEFAULT CHARACTER SET utf8mb4;
 
 GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
-GRANT ALL ON mysql.* TO 'root'@'localhost';
+GRANT ALL ON db.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
 
-USE `mysql`;
+USE `db`;
